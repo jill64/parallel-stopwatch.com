d85e3d94c6c2a65e5ca4ebd0d2a07a84b7e7ca15
jill64 <143883742+jill64@users.noreply.github.com>
2023-10-01 09:32:41 +0000

    fix: import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from 'tailwindcss'
 import gridAuto from '@jill64/tailwind-grid-auto'
-import reactions from '@jill64/tailwind-grid-auto'
+import reactions from '@jill64/tailwind-reactions'
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
   plugins: [gridAuto, reactions]
 } satisfies Config
