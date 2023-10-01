8970d136057b2ec3694b4d1f9055d46256cd0851
jill64 <143883742+jill64@users.noreply.github.com>
2023-10-01 18:26:00 +0900

    Update and rename tailwind.config.cjs to tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+import gridAuto from '@jill64/tailwind-grid-auto'
+import reactions from '@jill64/tailwind-grid-auto'
+
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [gridAuto, reactions],
+} satisfies Config
