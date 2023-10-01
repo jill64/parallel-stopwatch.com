aa70d63f69dbb7c9a5975a4300e2d5db30e63146
jill64 <143883742+jill64@users.noreply.github.com>
2023-10-01 18:13:38 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  test: {
-    include: ['./test/*.test.{js,ts}']
-  }
+  plugins: [sveltekit()]
 })
