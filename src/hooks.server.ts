a1dd087fb6881a64d2f8c40344ebed8bc652d698
jill64 <intents.turrets0h@icloud.com>
2023-10-20 11:49:25 +0900

    ref: sentry-svk-cf for error logging

---
+++ src/hooks.server.ts
@@ -0,0 +1,10 @@
+import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
+import { PUBLIC_SENTRY_DSN } from '$env/static/public'
+
+const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
+
+export const handle = onHandle()
+
+export const handleError = onError((e) => {
+  console.error(e)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [sveltekit()],
+  ssr: {
+    noExternal: ['@jill64/sentry-sveltekit-cloudflare']
+  },
   test: {
     include: ['./test/*.test.{js,ts}']
   }
