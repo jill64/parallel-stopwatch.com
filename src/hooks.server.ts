d270a130ba3bb6fc055d27e48fe136176b6b5892
jill64 <intents.turrets0h@icloud.com>
2023-11-01 21:01:26 +0900

    improve i18n

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,8 @@
-import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
 import { PUBLIC_SENTRY_DSN } from '$env/static/public'
+import { attach } from '$lib/i18n'
+import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
 
 const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
 
-export const handle = onHandle()
+export const handle = onHandle(attach)
 export const handleError = onError()
