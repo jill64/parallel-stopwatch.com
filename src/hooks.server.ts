fdd49cd1cbd693da199ff5e14480fa634b6238b0
jill64 <intents.turrets0h@icloud.com>
2023-11-04 23:52:53 +0900

    Add @jill64/svelte-ogp package and implement Open Graph Protocol metadata.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,10 @@
 import { PUBLIC_SENTRY_DSN } from '$env/static/public'
-import { attach } from '$lib/i18n'
+import { attach as lang_attach } from '$lib/i18n'
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
+import { attach as ogp_attach } from '@jill64/svelte-ogp'
+import { sequence } from '@sveltejs/kit/hooks'
 
 const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
 
-export const handle = onHandle(attach)
+export const handle = onHandle(sequence(lang_attach, ogp_attach))
 export const handleError = onError()
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,8 @@
   import { page } from '$app/stores'
   import { translate } from '$lib/i18n'
   import { isDark } from '@jill64/svelte-device-theme'
-  import { LocaleAlternates, LanguageManager } from '@jill64/svelte-i18n'
+  import { LanguageManager, LocaleAlternates } from '@jill64/svelte-i18n'
+  import { OGP } from '@jill64/svelte-ogp'
   import { Toaster } from '@jill64/svelte-toast'
   import '../app.postcss'
 
@@ -27,14 +28,16 @@
 <Toaster position="bottom-right" />
 <LanguageManager />
 <LocaleAlternates xDefaultHref={origin} />
+<OGP
+  {title}
+  {description}
+  site_name={title}
+  image="https://parallel-stopwatch.com/og-image.png"
+/>
 <svelte:head>
   <link rel="icon" href="{base}/favicon{suffix}.png" />
   <link rel="icon" href="{base}/favicon{suffix}.svg" type="image/svg+xml" />
   <link rel="apple-touch-icon" href="{base}/apple-touch-icon{suffix}.png" />
   <meta name="description" content={description} />
-  <meta property="og:url" content={href} />
-  <meta property="og:title" content={title} />
-  <meta property="og:description" content={description} />
-  <meta property="og:site_name" content={title} />
 </svelte:head>
 <slot />
