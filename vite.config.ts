c40c1efe741a6e0759994ac31e2a809d26e84ba6
jill64 <143883742+jill64@users.noreply.github.com>
2023-12-14 23:38:12 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [sveltekit()],
-  ssr: {
-    noExternal: ['@jill64/sentry-sveltekit-cloudflare']
-  },
   test: {
     include: ['**/*.test.ts']
   }
